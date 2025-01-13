fe6d1af68dc8f2171132cc6bc4764b155705f924
shenlehan233 <shenlehan233@gmail.com>
2025-01-13 17:40:00 +0800

    upload code for lecture 1

---
+++ code/example.sh
@@ -0,0 +1,11 @@
+echo "Starting the program at $(date)"
+
+echo "Running program $0 with $# arguments with pid $$"
+
+for file in "$@"; do
+  grep foobar "$file" > /dev/null 2> /dev/null
+  if [["$?" -ne 0]]; then
+    echo "File $file do not have any foobar, adding one"
+    echo "# foobar" >> "$file"
+  fi
+done
